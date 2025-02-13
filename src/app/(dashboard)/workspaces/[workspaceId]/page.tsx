8d15ab36d0381460cc9ed9b856ad7c981fc3dfa4
wangjian <wangjian0504@gmail.com>
2025-02-13 14:53:26 +0800

    退出登录检测并跳转

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,9 @@
-const WorkspaceIdPage = () => {
-  // 代码点击处理事件
+import { getCurrent } from '@/features/auth/actions'
+import { redirect } from 'next/navigation'
+
+const WorkspaceIdPage = async () => {
+  const user = await getCurrent()
+  if (!user) redirect('/sign-in')
 
   return <div>WorkspaceIdPage</div>
 }
--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,9 +1,16 @@
+import { getCurrent } from '@/features/auth/actions'
 import CreateWorkspaceForm from '@/features/workspaces/components/create-workspace-form'
+import { redirect } from 'next/navigation'
 
-export default function CreateWorkspacePage() {
+const CreateWorkspacePage = async () => {
+  const user = await getCurrent()
+  if (!user) redirect('/sign-in')
+
   return (
     <div className="w-full lg:max-w-xl">
       <CreateWorkspaceForm />
     </div>
   )
 }
+
+export default CreateWorkspacePage
